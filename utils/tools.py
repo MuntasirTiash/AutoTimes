da7c9a1a57cb2c4aced240ff2deb5f938f091fe7
Muntasir Shohrab <muntasir.tiash@gmail.com>
2025-11-11 10:46:27 -0500

    tools cahnge

--- utils/tools.py
+++ utils/tools.py
@@ -31,7 +31,7 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.inf
         self.delta = delta
         self.use_multi_gpu = args.use_multi_gpu
         if self.use_multi_gpu:
